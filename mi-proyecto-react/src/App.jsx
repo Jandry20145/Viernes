569f92ed7b8280200a6274ababe549dee3a2aa9f
Jandry <yamdrydamir04@gmail.com>
2025-06-20 16:28:54 -0500

    updated

---
+++ mi-proyecto-react/src/App.jsx
@@ -0,0 +1,11 @@
+import Footer from "./components/footer";
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <h1>Hola mundo soy Jandry Damir Solorzano Espin</h1>
+      <p>mi vida es el lol</p>
+      <Footer />
+    </div>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ export default function App() {
   return (
     <>
       <div>
-        <h1>Hola mundo bonito</h1>
-        <p>mi vida es una pesadilla</p>
+        <h1>Hola mundo soy Jandry Damir Solorzano Espin</h1>
+        <p>mi vida es aburrila</p>
         <Footer />
       </div>
     </>
